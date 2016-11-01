76d4ef6736213938510e81658a01ecd0cbdade2c
Richard Oh <rso136@gmail.com>
2016-10-31 22:29:26 -0400

    Updated friends.js

--- data/friends.js
+++ data/friends.js
@@ -3,7 +3,7 @@ var friends = [
 		name: 'Johnny',
 		photo: "http://1aon88369c3gtkxjg19cnprs.wpengine.netdna-cdn.com/wp-content/uploads/2016/02/thumbRNS-DEPP-REASON022416-262x369.jpg",
 		bio: "Some would say I am a thrill seeking personality. In reality, I have an introspective side of me which people rarely see. Even though I love the crowd and taking risks, I am a true introvert who appreciates profound philosophical conversations about life.",
-		email: "placeholder@none.com",
+		email: "Johnny@email.com",
 		scores: [
 			"3",
 			"2",
@@ -22,7 +22,7 @@ var friends = [
 		name: 'Glenn',
 		photo: "http://www.comicbookreligion.com/img/g/l/Glenn_Rhee_2.jpg",
 		bio: "I like battling the walking dead. Let's chill while barely evading the ravenous horde of biters that pursue me all day, everyday. I think you'll find that even though I'm a little on the awkward side, it really does add to my charm. Everybody likes me, dude.",
-		email: "placeholder@none.com",
+		email: "Glenn@email.com",
 		scores: [
 			"2",
 			"5",
@@ -41,7 +41,7 @@ var friends = [
 		name: 'Arnold',
 		photo: "https://s-media-cache-ak0.pinimg.com/236x/39/9e/a9/399ea939da7e542966ffb665f1cfe0b4.jpg",
 		bio: "I'm an outstanding individual who knows how to get along with people. Trust me, I never have a dull moment. I like to go fishing, play ping pong, and collect 9mm bullets. Sometimes, I like to hit the gym. Let's pump iron together.",
-		email: "placeholder@none.com",
+		email: "Arnold@email.com",
 		scores: [
 			"5",
 			"1",
